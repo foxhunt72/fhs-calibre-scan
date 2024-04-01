a4d1751bd5a6d92538b25b92c5dbdca40b67aacb
Richard de Vos <richard.de.vos@sentia.com>
2024-04-01 23:08:40 +0200

    fix directory permissions

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,6 +7,10 @@ USER_ID=${USER_ID:-1000}
 USER_NAME="appuser"
 adduser --disabled-password --gecos '' --uid "$USER_ID" "$USER_NAME"
 
+test -d /logs || mkdir /logs
+chown "$USER_NAME" /logs
+chown "$USER_NAME" /library
+
 # Run the script as the created user
 chmod a+x /opt/scan-calibre.sh
 echo "starting scan."
